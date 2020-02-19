32b42dc08f69dadb4765af0b212971229c6053e7
Jennifer Bland <ratracegrad@gmail.com>
2020-02-19 12:12:54 -0500

    Use SSL and the Puma webserver in production

--- config/puma.rb
+++ config/puma.rb
@@ -34,5 +34,23 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
+# Code to use puma when deployed to Heroku
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/
+  # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
