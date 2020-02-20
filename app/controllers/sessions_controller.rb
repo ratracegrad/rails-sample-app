91c1d963a1df1cadc087235bd314f5d53cdb0efb
Jennifer Bland <ratracegrad@gmail.com>
2020-02-19 22:26:45 -0500

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  # include SessionsHelper
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      # Create an error message
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,31 @@
+module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+
+    elsif (user_id = cookies.signed[:user_id])
+      # raise
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+end
