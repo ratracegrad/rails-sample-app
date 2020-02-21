11edbddbfe15711cb889bb9490d5383dda201e71
Jennifer Bland <ratracegrad@gmail.com>
2020-02-20 22:29:16 -0500

    Added account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_003202) do
+ActiveRecord::Schema.define(version: 2020_02_21_021203) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_02_21_003202) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
   end
 
 end
